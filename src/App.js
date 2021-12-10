dc7ee289732734def9235a1e4a07e4b00e0aa5cd
Vihrogonov <vihrogonov@protonmail.com>
2021-12-10 10:57:06 +0000

    Fetch Data Function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './App.css'
+
+import { fetchWeather } from './api/fetchWeather'
 
 const App = () => {
-  return <div>App</div>
+  const [query, setQuery] = useState('')
+
+  // fetch the data after we click something
+  const search = async (e) => {
+    if (e.key === 'Enter') {
+      const data = await fetchWeather(query)
+      console.log(data)
+    }
+  }
+
+  return (
+    <div className='main-container'>
+      <input
+        type='text'
+        className='search'
+        placeholder='Search...'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+    </div>
+  )
 }
 
 export default App
--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,1 +1,19 @@
+import axios from 'axios'
 
+const URL = 'https://api.openweathermap.org/data/2.5/weather'
+const API_KEY = '364c9d5917e8424053360fb7d0901710'
+
+// fetch data function
+// query = the name of the town we wont to search for.
+ export const fetchWeather = async (query) => {
+  // get response once we make a call to the URL of the API. Destructure the data from the response.
+  const response = await axios.get(URL, {
+    params: {
+      q: query,
+      units: 'metric',
+      APPID: API_KEY,
+    },
+  })
+
+  return response
+}
