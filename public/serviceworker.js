28eb982f43ae3598f0bcf8735416eae80d529a4e
Vihrogonov <vihrogonov@protonmail.com>
2021-12-13 10:49:47 +0000

    Create serviceworker.js

---
+++ public/serviceworker.js
@@ -0,0 +1,47 @@
+// create a cash name
+const CACHE_NAME = 'version-1'
+// create a variable that will be store index.js and the name of the file if there is no connection.
+const urlsToCache = ['index.html', 'offline.html']
+
+const self = this
+
+// write events for:
+// Install a SW
+self.addEventListener('install', (event) => {
+  // open the cache and we go add urlsToCache to the cache
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache!')
+      return cache.addAll(urlsToCache)
+    })
+  )
+})
+
+// Listen for request
+self.addEventListener('fetch', (event) => {
+  // do something with the request after we listen for them.
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match('offline.html'))
+    })
+  )
+})
+
+// Activate the SW
+self.addEventListener('activate', (event) => {
+  // activate event listener
+  const cacheWhitelist = []
+  cacheWhitelist.push(CACHE_NAME)
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    )
+  )
+})
